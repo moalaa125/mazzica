81df1a6a5eb6069e11d0367b6f94c3eaab57dc5e
moalaa125 <muhamedawadkassem@gmail.com>
2026-09-23 03:36:19 +0300

    add missing audio_handler and update main.dart to complete audio_service integration

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,24 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:liquid_glass_widgets/liquid_glass_widgets.dart';
 import 'package:mazzica/constants/app_color.dart';
 import 'package:mazzica/home_screen.dart';
+import 'package:audio_service/audio_service.dart';
+import 'package:mazzica/widgets/audio_handler.dart';
+
+late MyAudioHandler audioHandler;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await LiquidGlassWidgets.initialize();
+
+  audioHandler = await AudioService.init(
+    builder: () => MyAudioHandler(),
+    config: const AudioServiceConfig(
+      androidNotificationChannelId: 'com.mazzica.channel.audio',
+      androidNotificationChannelName: 'Audio playback',
+      androidNotificationOngoing: true,
+      androidStopForegroundOnPause: true,
+    ),
+  );
 
   runApp(
     LiquidGlassWidgets.wrap(
@@ -39,7 +53,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize: const Size(412, 915), 
+      designSize: const Size(412, 915),
       minTextAdapt: true,
       splitScreenMode: true,
       builder: (context, child) {
